a005520456f0eb3a1edc673a334cafa96fabedbc
pedrosilva2703 <up201806526@fe.up.pt>
2023-04-24 19:19:02 +0100

    OPCUA Test - connection, read and write

---
+++ src/main/java/com/example/mini_mes/App.java
@@ -0,0 +1,9 @@
+package com.example.mini_mes;
+
+public class App {
+
+    public static void main(String[] args){
+        Launcher.main(args);
+    }
+
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,12 @@
 module com.example.mini_mes {
     requires javafx.controls;
     requires javafx.fxml;
+    requires javafx.graphics;
+    requires java.sql;
+    requires postgresql;
+    requires java.prefs;
+    requires org.eclipse.milo.opcua.sdk.client;
+    requires org.eclipse.milo.opcua.stack.core;
 
 
     opens com.example.mini_mes to javafx.fxml;
